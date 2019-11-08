f7f8577286bfee15f175c7b7ed815ad470af99fe
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2019-11-08 07:35:07 -0700

    cha cha changes

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const path = require('path');
 
 const main = async () => {
   const args = yargs
-    .option('--test', {
-      alias: 'test',
+    .option('split', {
+      alias: 's',
       type: 'boolean',
-      describe: 'testing',
+      describe: 'split stdin by newlines into array of strings',
+      coerce: arg => typeof(arg) !== undefined,
+    })
+    .option('json', {
+      type: 'boolean',
+      describe: 'Whether or not to return json',
+      coerce: arg => typeof(arg) !== undefined,
+    })
+    .option('rawJson', {
+      alias: 'raw-json',
+      type: 'boolean',
+      describe: 'Whether or not to return as raw json string',
       coerce: arg => typeof(arg) !== undefined,
     })
     .argv;
@@ -22,16 +33,31 @@ const main = async () => {
     }
   }
 
-  /* eslint-disable-next-line */
-  const stdin = fs.readFileSync(0, 'utf8');
+  const stdin = getStdin(args);
 
   const stringFuncs = args._;
 
   const result = stringFuncs.reduce((result, stringFunc) => (
     eval(stringFunc)(result)
-  ), stdin.trim());
+  ), stdin);
 
-  console.log(result);
+  printFormatted(args, result);
+}
+
+const getStdin = args => {
+  const rawStdin = fs.readFileSync(0, 'utf8');
+
+  return args.split ? rawStdin.trim().split("\n") : rawStdin.trim();
+}
+
+const printFormatted = (args, result) => {
+  if(args.json) {
+    console.log(JSON.stringify(result));
+  } else if(args.rawJson) {
+    console.log(JSON.stringify(JSON.stringify(result)));
+  } else {
+    console.log(result)
+  }
 }
 
 main();
